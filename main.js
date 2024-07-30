b087d89c74238f648439ddc4c65d00d981c2593b
Alison Adriel <164245040+alisonadriel@users.noreply.github.com>
2024-07-30 13:51:38 -0300

    add: interatividade-menu

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//navbar toggle
+// interações do menu
 
 const navOpenBtn = document.querySelector('.nav-open-btn');
 const navBar = document.querySelector('.navbar');
@@ -11,6 +11,17 @@ const elemArr = [navCloseBtn, overlay, navOpenBtn];
 
 for(let i = 0; i<elemArr.length; i++){
   elemArr[i].addEventListener("click", function(){
+    navBar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  })
+}
+
+const navBarLinks = document.querySelectorAll('.navbar-item');
+
+// Fazer o menu desaparecer quando clicar em algum link 
+
+for(let i=0; i < navBarLinks.length; i++){
+  navBarLinks[i].addEventListener("click", function(){
     navBar.classList.toggle("active");
     overlay.classList.toggle("active");
   })
