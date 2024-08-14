725c211e7a974e4a307dac58306de1cc0321aa0d
Alison Adriel <adriel.dias15@gmail.com>
2024-08-14 20:11:24 -0300

    add: copy-pop-up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -54,34 +54,4 @@ buttonClose.onclick = function () {
   modal.close();
 };
 
-const copyPix = document.querySelector(".chave-pix");
-const copyBtn = document.querySelector(".copy-btn");
-
-copyBtn.addEventListener("click", () => {
-  const pixText = copyPix.textContent;
-
-  navigator.clipboard.writeText(pixText).then(() => {
-    const popup = document.createElement("div");
-    popup.textContent = "Texto copiado com sucesso!";
-    popup.style.position = "fixed";
-    popup.style.top = "10px";
-    popup.style.left = "50%";
-    popup.style.transform = "translateX(-50%)";
-    popup.style.padding = "10px 20px";
-    popup.style.backgroundColor = "rgb(90, 16, 228)";
-    popup.style.color = "white";
-    popup.style.borderRadius = "5px";
-    popup.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.1)";
-    popup.style.zIndex = "9999";
-    popup.style.fontFamily = "Poppins, sans-serif";
-    popup.style.fontSize = "1.3rem";
-
-    document.body.appendChild(popup);
-
-    setTimeout(() => {
-      popup.remove();
-    }, 3000);
-  });
-});
-
 
---
+++ src/scripts/modal.js
@@ -0,0 +1,30 @@
+const copyPix = document.querySelector(".chave-pix");
+const copyBtn = document.querySelector(".copy-btn");
+const dialog = document.querySelector(".modal");
+
+copyBtn.addEventListener("click", () => {
+  const pixText = copyPix.textContent;
+
+  navigator.clipboard.writeText(pixText).then(() => {
+    const popup = document.createElement("div");
+    popup.textContent = "Texto copiado com sucesso!";
+    popup.style.position = "fixed";
+    popup.style.top = "10px";
+    popup.style.left = "50%";
+    popup.style.transform = "translateX(-50%)";
+    popup.style.padding = "10px 20px";
+    popup.style.backgroundColor = "rgb(90, 16, 228)";
+    popup.style.color = "white";
+    popup.style.borderRadius = "5px";
+    popup.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.1)";
+    popup.style.zIndex = "10";
+    popup.style.fontFamily = "Poppins, sans-serif";
+    popup.style.fontSize = "1.3rem";
+
+    dialog.appendChild(popup);
+
+    setTimeout(() => {
+      popup.remove();
+    }, 3000);
+  });
+});
