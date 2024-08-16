99cd0ae2f8594ac386ac12e11228bd2c968e26da
Alison Adriel <adriel.dias15@gmail.com>
2024-08-16 20:52:36 -0300

    feat: adding a load small screen

--- src/scripts/mail.js
+++ src/scripts/mail.js
@@ -11,11 +11,18 @@ const message = document.querySelector("#message");
 window.onload = function() {
   document.getElementById('contact-form').addEventListener('submit', function(event) {
       event.preventDefault();
-      // these IDs from the previous steps
+      document.querySelector(".loader").classList.add("show")
       emailjs.sendForm('service_5kl2b03', 'template_m2rj2tn', this)
-          .then(() => {
-              console.log('SUCCESS!');
-          }, (error) => {
+          .then(
+              function(){
+                document.querySelector(".contact-form").reset();
+                document.querySelector(".loader").classList.remove("show");
+                msg.innerHTML = "";
+                msg.innerHTML += "<span class='sucess-msg'> E-mail enviado!</span>";
+                msg.classList.add("show");
+                setTimeout(() => message.classList.remove("show"), 2000);
+            },
+           (error) => {
               console.log('FAILED...', error);
           });
   });
