d7baaee5ccecbf5264eb7f7c3b58a0a25c92bcc7
Alison Adriel <adriel.dias15@gmail.com>
2024-08-16 09:38:03 -0300

    feat: send-mail-suggest

--- src/scripts/mail.js
+++ src/scripts/mail.js
@@ -1,16 +1,22 @@
-function sendEmail() {
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-  Email.send({
-    SecureToken: "dc56002e-1f51-454d-9107-0b340ceb2e9a",
-    To: "allison.adriel21@gmail.com",
-    From: "allison.adriel21@gmail.com",
-    Subject: "Testando e-mail",
-    Body: `
-        Name: ${name} <br>
-        Email: ${email} <br>
-        Message: ${message}
-    `,
-  }).then((message) => alert(message));
+(function() {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init({
+    publicKey: "esxt9BHWFNLhDoDqG",
+  });
+})();
+
+const message = document.querySelector("#message");
+
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // these IDs from the previous steps
+      emailjs.sendForm('service_5kl2b03', 'template_m2rj2tn', this)
+          .then(() => {
+              console.log('SUCCESS!');
+          }, (error) => {
+              console.log('FAILED...', error);
+          });
+  });
 }
