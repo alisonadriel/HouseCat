0c845bc54d28c8e5eb2391cc9ce855b9b5b16d4b
Alison Adriel <adriel.dias15@gmail.com>
2024-08-15 21:35:05 -0300

    add: send-e-mail

--- src/scripts/mail.js
+++ src/scripts/mail.js
@@ -1,13 +1,16 @@
- function sendEmail(){
-    Email.send({
-      Host : "smtp.gmail.com",
-      Username : "username",
-      Password : "password",
-      To : 'them@website.com',
-      From : "you@isp.com",
-      Subject : "This is the subject",
-      Body : "And this is the body"
-}).then(
-message => alert(message)
-  ); 
+function sendEmail() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+  Email.send({
+    SecureToken: "dc56002e-1f51-454d-9107-0b340ceb2e9a",
+    To: "allison.adriel21@gmail.com",
+    From: "allison.adriel21@gmail.com",
+    Subject: "Testando e-mail",
+    Body: `
+        Name: ${name} <br>
+        Email: ${email} <br>
+        Message: ${message}
+    `,
+  }).then((message) => alert(message));
 }
