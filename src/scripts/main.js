e0528e29b4c8ceade58b8ea95f18526bce088237
Alison Adriel <adriel.dias15@gmail.com>
2024-08-21 21:20:52 -0300

    style: menu-height-fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -69,16 +69,14 @@ apoieBtn.addEventListener("click", () => {
 const sections = document.querySelectorAll(".js-scroll");
 const windowMetade = window.innerHeight * 0.6;
 
-function animaScroll(){
-  sections.forEach((section)=>{
+function animaScroll() {
+  sections.forEach((section) => {
     const sectionTop = section.getBoundingClientRect().top;
     const isSectionVisible = (sectionTop - windowMetade) < 0;
-    if(isSectionVisible){
+    if (isSectionVisible) {
       section.classList.add("ativo");
     }
-  })
+  });
 }
 
-
 window.addEventListener("scroll", animaScroll);
-
